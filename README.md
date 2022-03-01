2ea6e5419a796404857489a03d18000f25f42689
Miguel Grinberg <miguel.grinberg@gmail.com>
2022-03-01 15:59:38 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+twilio-serverless-video
+=======================
+
+A serverless video chat application powered by [Twilio Video](https://www.twilio.com/video).
---
+++ assets/app.js
@@ -0,0 +1,115 @@
+const usernameInput = document.getElementById('username');
+const button = document.getElementById('join_leave');
+const container = document.getElementById('container');
+const count = document.getElementById('count');
+let connected = false;
+let room;
+
+const addLocalVideo = async () => {
+  const track = await Twilio.Video.createLocalVideoTrack();
+  const video = document.getElementById('local').firstElementChild;
+  video.appendChild(track.attach());
+};
+
+const connectButtonHandler = async (event) => {
+  event.preventDefault();
+  if (!connected) {
+    const username = usernameInput.value;
+    if (!username) {
+      alert('Enter your name before connecting');
+      return;
+    }
+    button.disabled = true;
+    button.innerHTML = 'Connecting...';
+    try {
+      await connect(username);
+      button.innerHTML = 'Leave call';
+      button.disabled = false;
+    }
+    catch {
+      alert('Connection failed. Is the backend running?');
+      button.innerHTML = 'Join call';
+      button.disabled = false;    
+    }
+  }
+  else {
+    disconnect();
+    button.innerHTML = 'Join call';
+    connected = false;
+  }
+};
+
+const connect = async (username) => {
+  const response = await fetch('/get_token', {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({'username': username}),
+  });
+  const data = await response.json();
+  room = await Twilio.Video.connect(data.token);
+  room.participants.forEach(participantConnected);
+  room.on('participantConnected', participantConnected);
+  room.on('participantDisconnected', participantDisconnected);
+  connected = true;
+  updateParticipantCount();
+};
+
+const updateParticipantCount = () => {
+  if (!connected) {
+    count.innerHTML = 'Disconnected.';
+  }
+  else {
+    count.innerHTML = (room.participants.size + 1) + ' participants online.';
+  }
+};
+
+const participantConnected = (participant) => {
+  const participantDiv = document.createElement('div');
+  participantDiv.setAttribute('id', participant.sid);
+  participantDiv.setAttribute('class', 'participant');
+
+  const tracksDiv = document.createElement('div');
+  participantDiv.appendChild(tracksDiv);
+
+  const labelDiv = document.createElement('div');
+  labelDiv.innerHTML = participant.identity;
+  participantDiv.appendChild(labelDiv);
+
+  container.appendChild(participantDiv);
+
+  participant.tracks.forEach(publication => {
+    if (publication.isSubscribed) {
+      trackSubscribed(tracks_div, publication.track);
+    }
+  });
+  participant.on('trackSubscribed', track => trackSubscribed(tracksDiv, track));
+  participant.on('trackUnsubscribed', trackUnsubscribed);
+  updateParticipantCount();
+};
+
+const participantDisconnected = (participant) => {
+  document.getElementById(participant.sid).remove();
+  updateParticipantCount();
+};
+
+const trackSubscribed = (div, track) => {
+  div.appendChild(track.attach());
+};
+
+const trackUnsubscribed = (track) => {
+  track.detach().forEach(element => element.remove());
+};
+
+const disconnect = () => {
+  room.disconnect();
+  while (container.lastChild.id != 'local') {
+      container.removeChild(container.lastChild);
+  }
+  button.innerHTML = 'Join call';
+  connected = false;
+  updateParticipantCount();
+};
+
+addLocalVideo();
+button.addEventListener('click', connectButtonHandler);
+
